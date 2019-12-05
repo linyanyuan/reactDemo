f8b64cc13ad19e20aa2014789069c8a635d8510b
linyanyuan <2769039876@qq.com>
2019-12-05 20:12:08 +0800

    dev分支测试

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './style.css'
-console.log('sdsfsfsdfsdfsdfsagfdsag')
+console.log('我是dev分支')
 const ele = React.createElement('div',{className:'hello'},'士大夫萨芬')
 ReactDOM.render(ele,document.getElementById("app"))
