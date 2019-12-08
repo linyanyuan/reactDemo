ba334a7d27cb63a38352f70d08c2a95860843af1
linyanyuan <2769039876@qq.com>
2019-12-08 13:48:02 +0800

    Login页面

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,17 +1,61 @@
 // 登录组件
 import React,{Component} from 'react'
-import {Row, Col} from 'antd'
+import {Row, Col, Icon, Form, Input,Button,Checkbox } from 'antd'
 import './Login.css'
 export default class Login extends Component {
     render(){
+        const { getFieldDecorator } = this.props.form;
         return(
             <Row className="mainBox">
                 <Col xs={0} sm={14} md={14} lg={14} xl={14} className="leftImage"></Col>
-                <Col xs={24} sm={10} md={10} lg={10} xl={10}>
-                    
+                <Col xs={24} sm={10} md={10} lg={10} xl={10}  className="rightContent">
+                    <div className="contentBox">
+                        <Icon type="meh" theme="twoTone" style={{ fontSize: '40px',margin:'8px'}} twoToneColor="rgb(220, 0, 78)"/>
+                        <Col style={{fontSize:'1.5rem',fontWeight: '400',lineHeight: '1.33'}}>Sign in</Col>
+                        <Col lg={16} xl={16} style={{marginTop: '40px'}}>
+                            <Form className="login-form">
+                                <Form.Item>
+                                    {getFieldDecorator('username',{
+                                        rules:[{ required: true, message: 'Please input your username!' }]
+                                    })(
+                                        <Input 
+                                        size="large"
+                                        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)'}} />} 
+                                        placeholder="Username">
+                                        </Input>
+                                    )}
+                                </Form.Item>
+                                <Form.Item>
+                                    {getFieldDecorator('password',{
+                                        rules:[{ required: true, message: 'Please input your Password!' }]
+                                    })(
+                                        <Input 
+                                        size="large"
+                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)'}} />} 
+                                        placeholder="Password">
+                                        </Input>
+                                    )}
+                                </Form.Item>
+                                <Form.Item>
+                                    {getFieldDecorator('remember', {
+                                        valuePropName: 'checked',
+                                        initialValue: true,
+                                    })(<Checkbox>Remember me</Checkbox>)}
+                                    <a className="login-form-forgot" href="">
+                                        Forgot password
+                                    </a>
+                                    <Button type="primary" htmlType="submit" className="login-form-button">
+                                        Log in
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        </Col>
+                        
+                    </div>
                 </Col>
             </Row>
         )
     }
 }
+Login = Form.create({})(Login)
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Router from './config/Route' // 引入路由表
-import App from './App' // 引入根组件
 // import zhCN from 'antd/es/locale/zh_CN';
 // import 'antd/dist/antd.css'; // 引入antd.css
 import './style.css'
