1708ae79ac807f0393f72fd60db45d995f52f537
linyanyuan <2769039876@qq.com>
2022-01-26 10:48:15 +0800

    测试

--- src/api/api.js
+++ src/api/api.js
@@ -5,3 +5,5 @@ export const deleteAccount = `/api/deleteAccount`; // 删除一条
 export const updateAccount = `/api/updateAccount`; // 修改
 export const queryStatistics = `/api/queryStatistics`; // 获取本月或本年统计信息
 // 测试一下git
+
+//在测试一下
